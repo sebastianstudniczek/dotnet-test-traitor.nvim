648fc89952309f9778c8ef1410f04018c35f3790
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-07-12 00:11:12 +0200

    Use vim.ui.select for picker

--- lua/dotnet-test-traitor/picker.lua
+++ lua/dotnet-test-traitor/picker.lua
@@ -1,54 +1,17 @@
 local M = {}
 
----@param cb fun(filter: string) Callback to handle the selected filter
+---@param cb fun(filter: dotnet-test-traitor.TestFilter) Callback to handle the selected filter
 M.pick_filter = function(cb)
   local opts = require("dotnet-test-traitor").opts
 
-  ---@type snacks.picker.finder.Item[]
-  local picker_items = {}
-
-  for i, filter in ipairs(opts.filters) do
-    table.insert(picker_items, {
-      idx = i,
-      formatted = filter.name,
-      text = i .. " " .. filter.name,
-      value = filter.value,
-    })
-  end
-
-  local padding = 15
-
-  require("snacks").picker.pick(
-    ---@type snacks.picker.Config
-    {
-      source = "Test filters",
-      items = picker_items,
-      format = function(item, _)
-        ---@type snacks.picker.Highlight[]
-        local ret = {}
-        local idx = tostring(item.idx)
-        ret[#ret + 1] = { idx .. ".", "SnacksPickerIdx" }
-        ret[#ret + 1] = { " " }
-        ret[#ret + 1] = { item.formatted, "SnacksPickerLabel" }
-        ret[#ret + 1] = { string.rep(" ", padding - #item.text), virtual = true }
-        ret[#ret + 1] = { item.value, "SnacksPickerComment" }
-        return ret
-      end,
-      confirm = function(picker, item)
-        picker:close()
-        cb(item.value)
-      end,
-      layout = {
-        preview = false,
-        layout = {
-          width = 0.4,
-          min_width = 80,
-          height = 0.2,
-          min_height = 10,
-        },
-      },
-    }
-  )
+  vim.ui.select(opts.filters, {
+    prompt = "Select filter:",
+    format_item = function(item)
+      return ("%s %s"):format(item.name, item.value)
+    end,
+  }, function(choice)
+    cb(choice)
+  end)
 end
 
 return M
